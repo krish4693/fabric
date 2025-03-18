60e48597f3c8c30a51b96a93a10f5876bbd2e955
Krishnanand <krishnanandunnikrishnan@gmail.com>
2025-03-18 20:15:45 +0530

    feat: circle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Canvas,Rect } from "fabric";
+import { Canvas,Rect,Circle } from "fabric";
 import Image from "next/image";
 import { useRef, useState, useEffect } from "react";
 import { IconButton } from "@mui/material"; // MUI IconButton alternative
 import SquareIcon from "@mui/icons-material/Square"; // MUI SquareIcon alternative
+import CircleIcon from "@mui/icons-material/Circle"; // MUI SquareIcon alternative
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -38,11 +39,27 @@ export default function Home() {
     }
     };
 
+    const addCircle = () => {
+      if (canvas){
+      const circle = new Circle({
+      top: 150,
+      left: 150,
+      radius: 50,
+      fill: "#084D42",
+      });
+      canvas.add(circle);
+      }
+      };
+    
+
   return (
     <div className="App">
       <div className="Toolbar darkmode">
         <IconButton onClick={addRectangle} color="primary">
           <SquareIcon />
+        </IconButton>
+        <IconButton onClick={addCircle} color="primary">
+          <CircleIcon/>
         </IconButton>
       </div>
       <canvas id="canvas" ref={canvasRef} />
